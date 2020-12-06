03cd20607222400ee4727f8fad856f51b6d8dba8
马明聪 <hellomamingcong@163.com>
2020-12-06 11:00:17 +0800

    可预约医生

--- src/main/java/com/lrm/hospital/config/CorsConfig.java
+++ src/main/java/com/lrm/hospital/config/CorsConfig.java
@@ -1,33 +1,33 @@
-package com.lrm.hospital.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-/**
- * @ClassName CorsConfig
- * @Description 跨域配置
- * @Author mamingcong
- * @Date 2020/5/25 10:00
- * @Version 1.0
- */
-@Configuration
-public class CorsConfig {
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*"); //允许任何域名
-        corsConfiguration.addAllowedHeader("*"); //允许任何头
-        corsConfiguration.addAllowedMethod("*"); //允许任何方法
-        corsConfiguration.setAllowCredentials(true);
-        return corsConfiguration;
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig()); //注册
-        return new CorsFilter(source);
-    }
-}
+//package com.lrm.hospital.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+///**
+// * @ClassName CorsConfig
+// * @Description 跨域配置
+// * @Author mamingcong
+// * @Date 2020/5/25 10:00
+// * @Version 1.0
+// */
+//@Configuration
+//public class CorsConfig {
+//    private CorsConfiguration buildConfig() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.addAllowedOrigin("*"); //允许任何域名
+//        corsConfiguration.addAllowedHeader("*"); //允许任何头
+//        corsConfiguration.addAllowedMethod("*"); //允许任何方法
+//        corsConfiguration.setAllowCredentials(true);
+//        return corsConfiguration;
+//    }
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", buildConfig()); //注册
+//        return new CorsFilter(source);
+//    }
+//}
